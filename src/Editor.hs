dd49e265a784bc9d2e6730cce4f57c1c90eefc5c
Johan Holmquist <holmisen@gmail.com>
2016-12-01 19:05:12 +0100

    [Editor] Now using Selector

--- src/Editor.hs
+++ src/Editor.hs
@@ -4,8 +4,11 @@ module Editor where
 
 import Model
 import ModelRender (renderModel)
+import Selector (Selector)
 import Types
+import qualified Selector
 
+import Data.Foldable
 import Graphics.Rendering.OpenGL as GL
 import Lens.Micro
 import Lens.Micro.Extras (view)
@@ -15,39 +18,39 @@ import Lens.Micro.TH
 
 data Editor
    = Place [PlacedPart] [PlacedPart]
-   | Parts [PlacedPart]
+   | Parts (Selector PlacedPart)
    deriving Show
 
 placedParts :: Editor -> [PlacedPart]
 placedParts (Place _ placedParts) = placedParts
-placedParts (Parts ps) = ps
+placedParts (Parts ps) = toList ps
 
 partsToPlace :: Editor -> [PlacedPart]
 partsToPlace (Place ps _) = ps
-partsToPlace (Parts ps)   = ps
+partsToPlace (Parts _)    = []
 
 --------------------------------------------------------------------------------
 
 placeParts :: [PlacedPart] -> Editor -> Editor
 placeParts toPlace (Place _ placed) = Place toPlace placed
-placeParts toPlace (Parts placed)   = Place toPlace placed
+placeParts toPlace (Parts placed)   = Place toPlace $ toList placed
 
 --------------------------------------------------------------------------------
 
 -- Not used now -- should possibly not be in this module
-renderEditor :: Editor -> IO ()
-renderEditor (Place toPlace placed) = do
+-- renderEditor :: Editor -> IO ()
+-- renderEditor (Place toPlace placed) = do
 
-   -- Render wireframe for parts to place
-   GL.polygonMode $= (GL.Line, GL.Line)
-   renderModel toPlace
+--    -- Render wireframe for parts to place
+--    GL.polygonMode $= (GL.Line, GL.Line)
+--    renderModel toPlace
 
-   -- Render placed parts "normally"
-   GL.polygonMode $= (GL.Fill, GL.Fill)
-   renderModel placed
+--    -- Render placed parts "normally"
+--    GL.polygonMode $= (GL.Fill, GL.Fill)
+--    renderModel placed
 
-renderEditor (Parts parts) = do
-   GL.polygonMode $= (GL.Fill, GL.Fill)
-   renderModel parts
+-- renderEditor (Parts parts) = do
+--    GL.polygonMode $= (GL.Fill, GL.Fill)
+--    renderModel parts
 
 --------------------------------------------------------------------------------
