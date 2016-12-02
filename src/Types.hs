41701459d09d9626dc44cf0d7f9fcf75d6cbe7e3
Johan Holmquist <holmisen@gmail.com>
2016-12-02 23:51:20 +0100

    [Types] Added lenses for P3 components (not used for now)

--- src/Types.hs
+++ src/Types.hs
@@ -8,6 +8,7 @@ module Types
   , Vector3(..)
   , Vertex3(..)
   , P3(..)
+  , lposX, lposY, lposZ
   , V3
   , translate
   , vectorBetween
@@ -16,10 +17,21 @@ where
 
 import Graphics.Rendering.OpenGL (Color3(..), Color4(..), GLdouble, GLfloat, GLubyte, Vector3(..), Vertex3(..))
 import GLInstances ()
+import Lens.Micro
 
 type V3 = Vector3
 
 data P3 = P3 !Int !Int !Int deriving (Eq, Ord, Show)
+
+lposX :: Lens' P3 Int
+lposX = lens (\(P3 x _ _) -> x) (\(P3 _ y z) x -> P3 x y z)
+
+lposY :: Lens' P3 Int
+lposY = lens (\(P3 _ y _) -> y) (\(P3 x _ z) y -> P3 x y z)
+
+lposZ :: Lens' P3 Int
+lposZ = lens (\(P3 _ _ z) -> z) (\(P3 x y _) z -> P3 x y z)
+
 
 data Color
     = Black
