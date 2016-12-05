f0927729843f66084ba1f0d4aa967c9de539eee1
Johan Holmquist <holmisen@gmail.com>
2016-12-05 19:22:55 +0100

    [Types] Code cleanup

--- src/Types.hs
+++ src/Types.hs
@@ -1,43 +1,38 @@
 module Types
-  ( Color(..)
-  , Color3(..)
-  , Color4(..)
-  , GLdouble
-  , GLfloat
-  , GLubyte
-  , Vector3(..)
-  , Vertex3(..)
-  , P3(..)
-  , lposX, lposY, lposZ
-  , origo
-  , V3
-  , translate
-  , rotateZ
-  , vectorBetween
-  , vectorAdd
-  )
+   (
+   -- * Colors
+     Color(..)
+   , Color3(..)
+   , Color4(..)
+
+   -- * Scalars
+   , GLdouble
+   , GLfloat
+   , GLubyte
+
+   -- * Other
+   , Vector3(..)
+   , Vertex3(..)
+
+   -- * Positions
+   , P3(..)
+   , lposX, lposY, lposZ
+   , origo
+
+   -- * Vectors
+   , V3
+   , translate
+   , rotateZ
+   , vectorBetween
+   , vectorAdd
+   )
 where
 
 import Graphics.Rendering.OpenGL (Color3(..), Color4(..), GLdouble, GLfloat, GLubyte, Vector3(..), Vertex3(..))
 import GLInstances ()
 import Lens.Micro
 
-type V3 = Vector3
-
-data P3 = P3 !Int !Int !Int deriving (Eq, Ord, Show)
-
-origo :: P3
-origo = P3 0 0 0
-
-lposX :: Lens' P3 Int
-lposX = lens (\(P3 x _ _) -> x) (\(P3 _ y z) x -> P3 x y z)
-
-lposY :: Lens' P3 Int
-lposY = lens (\(P3 _ y _) -> y) (\(P3 x _ z) y -> P3 x y z)
-
-lposZ :: Lens' P3 Int
-lposZ = lens (\(P3 _ _ z) -> z) (\(P3 x y _) z -> P3 x y z)
-
+--------------------------------------------------------------------------------
 
 data Color
     = Black
@@ -55,6 +50,25 @@ data Color
     | Yellow
    deriving (Eq, Ord, Show)
 
+--------------------------------------------------------------------------------
+
+data P3 = P3 !Int !Int !Int deriving (Eq, Ord, Show)
+
+origo :: P3
+origo = P3 0 0 0
+
+lposX :: Lens' P3 Int
+lposX = lens (\(P3 x _ _) -> x) (\(P3 _ y z) x -> P3 x y z)
+
+lposY :: Lens' P3 Int
+lposY = lens (\(P3 _ y _) -> y) (\(P3 x _ z) y -> P3 x y z)
+
+lposZ :: Lens' P3 Int
+lposZ = lens (\(P3 _ _ z) -> z) (\(P3 x y _) z -> P3 x y z)
+
+--------------------------------------------------------------------------------
+
+type V3 = Vector3
 
 translate :: V3 Int -> P3 -> P3
 translate (Vector3 a b c) (P3 x y z) = P3 (x + a) (y + b) (z + c)
